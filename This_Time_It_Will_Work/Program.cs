09137b553ee5e4676a39639100bf25cf47b7d7cb
Vitaliy Khlebalov <63170601+vokhlebalov@users.noreply.github.com>
2020-12-20 19:31:14 +0500

    Little fix in QueryMaker

--- This_Time_It_Will_Work/Program.cs
+++ This_Time_It_Will_Work/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using QueryMaker;
+
 namespace This_Time_It_Will_Work
 {
     static class Program
@@ -16,7 +18,7 @@ namespace This_Time_It_Will_Work
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new TableChooserForm());
         }
     }
 }
